685d7c3f85a982c5538176bb73234780279b8cbd
Ihor Komar <kisenbenz8@gmail.com>
2023-02-13 16:52:58 +0200

    finish )

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galItemsMarkup = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galItemsMarkup);
+// gallery.addEventListener("click", onClick);
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 500,
+  animationSpeed: 2000,
+});
